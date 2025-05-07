ebe29372a74276b7f98aec7ea5214b1744ba4151
niikun <niikun0209@gmail.com>
2025-05-08 07:17:42 +0900

    fizzbuzz のサブスクライブまで作成

---
+++ src/chapter2/my_happy_fizz_buzz/my_happy_fizz_buzz/fizz_buzz_node.py
@@ -0,0 +1,32 @@
+import sys
+
+import rclpy
+from rclpy.node import Node 
+from std_msgs.msg import Int32,String
+from rclpy.executors import ExternalShutdownException
+
+class FizzBuzzNode(Node):
+    def __init__(self):
+        super().__init__('fizz_buzz')
+        self.pub = self.create_publisher(String,'happy_fiz_buz',10)
+        self.sub = self.create_subscription(Int32,'happy_num',self.callback,10)
+
+    def callback(self,sub_msg):
+        pub_msg = String()
+        self.get_logger().info(f'サブクライブ{sub_msg.data}')
+        # publish
+        # if sub_msg % 3 == 0 and  sub_msg % 5 == 0:
+        #     self.get_logger().info('') 
+
+def main():
+    rclpy.init()
+    node = FizzBuzzNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    except ExternalShutdownException:
+        sys.exit(1)
+    finally:
+        node.destroy_node()
+        rclpy.try_shutdown()
